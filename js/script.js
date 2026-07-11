98cfe64a77d777e7f61f1050f6abed120d489e55
aaronguilapps-lab <aaronguil.apps@gmail.com>
2026-07-11 10:12:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+function show(id){
+    document.querySelectorAll(".page").forEach(page=>{
+        page.classList.remove("active");
+    });
 
+    document.getElementById(id).classList.add("active");
+
+    const menu=document.getElementById("menu");
+    if(menu){
+        menu.classList.remove("show-menu");
+    }
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+}
+
+function toggleMenu(){
+    document.getElementById("menu").classList.toggle("show-menu");
+}
+
+function openLightbox(src){
+    document.getElementById("lightbox-img").src=src;
+    document.getElementById("lightbox").style.display="flex";
+}
+
+function closeLightbox(){
+    document.getElementById("lightbox").style.display="none";
+}
+
+window.onload=function(){
+    show("home");
+};
